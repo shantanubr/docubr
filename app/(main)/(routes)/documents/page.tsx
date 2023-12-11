70d739360a175f4ca68ceb7900c2fd73520ad73a
MonaLisa <mahaleshantanu3@gmail.com>
2023-12-11 23:53:22 +0530

    sidebar for navigation done

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Documents: React.FC = () => {
+  return <p>Documents page</p>;
+};
+
+export default Documents;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import React from "react";
+import Navigation from "./_components/navigation";
+
+const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  if (isLoading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+  return (
+    <div className="h-screen flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
